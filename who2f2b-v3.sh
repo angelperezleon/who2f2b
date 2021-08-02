09757033d9dfd7307df73a5fdfbbfd09facf6f12
angelperezleon <25584631+angelperezleon@users.noreply.github.com>
2021-08-02 18:42:29 +0200

    Create who2f2b-v3.sh

---
+++ who2f2b-v3.sh
@@ -0,0 +1,19 @@
+
+#!/bin/bash
+# file: who2f2b.sh
+# Source: $me
+# Credits: <jochum> libera.chat irc (2.8.2021)
+# Date: April 8th 2021 at CET 18:11
+# Description: zgrep fail2ban logs for any ban 2nd or 3rd octecs and compress older log
+
+ips=$(awk '($(NF-1) = /Ban/){print $NF}' /opt/log/fail2ban.log | sort | uniq | sort -n)
+
+for i in ${ips};
+do
+    ban=$(whois -h v4.whois.cymru.com " -c -p ${i}" | awk 'NR!=1{print $5}')
+    sleep 2
+    sudo fail2ban-client set sshd banip "${ban}";
+    #echo "sudo fail2ban-client set sshd banip ${ban}";
+done
+service fail2ban reload
+
